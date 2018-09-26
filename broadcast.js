c21e29449d89295c86463d6b3797b1c29f60c1d2
eweuweiueyweuyew <43505932+eweuweiueyweuyew@users.noreply.github.com>
2018-09-25 20:36:45 -0600

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -1,50 +1,38 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-var prefix = 'JB'
+var prefix = 'jp'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setStatus("dnd");
 });
 
-const moment = require("moment");
-client.on('message', async message => {
-    if (!message.channel.guild) return undefined;
-    let time = moment().format('Do MMMM YYYY , hh:mm');
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "bc")) {
-        if(!message.guild.member(message.author).hasPermission("MANAGE_SERVER")) return message.reply("**# You don't have the needed permissions!**");
-        if(!args) return message.reply("**# Supply a message!**");
-        message.channel.send(`\`\`- Name:\`\`\n${message.author}\n\n\`\`- Date:\`\`\n${time}\n\n\`\`- Message:\`\`\n${args}\n\n__# | You have 15s to say Yes or No__`)
-.then(() => {
-  message.channel.awaitMessages(response => response.content === 'yes', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-                let bcEmbed = new Discord.RichEmbed()
-          .setAuthor(`${message.author.username}#${message.author.discriminator}`,message.author.avatarURL)
-          .setThumbnail(message.guild.iconURL || message.guild.avatarURL)
-          .setColor('#000000')
-          .setColor('#36393e')
-          .addField('- Sender:',message.author)
-          .addField('- Server:',message.guild.name)
-          .addField('- Message:',`\`\`\`${args}\`\`\``);
-          message.guild.members.forEach(m => m.sendMessage(bcEmbed));
-          message.channel.send(`**Done!, Sent the message to: \`${message.guild.members.size}\` members!**`);
-      
-  });
-});
+client.on('message', message => {
+
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+message.channel.send("**jpbc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#000000')
+            .setColor('#36393e')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
     } else {
-          message.channel.awaitMessages(response => response.content === 'no', {
-    max: 1,
-    time: 15000,
-    errors: ['time'],
-  })
-  .then((collected) => {
-      message.channel.send("__- Canceled!__")
-    });
+        return;
     }
 });
 
