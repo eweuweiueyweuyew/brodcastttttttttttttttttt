866e2ec85320848995bcb4b4e2668e2fdf8c7f31
eweuweiueyweuyew <43505932+eweuweiueyweuyew@users.noreply.github.com>
2018-09-27 16:57:31 -0600

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-var prefix = 'jp'
+var prefix = '-'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setStatus("dnd");
